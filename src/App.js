badcb5924276555032824d06b8b41f6ef85e4907
Paul Ashby <paul@primitive.co>
2023-02-15 12:23:22 +0000

    Set destination when user makes selection

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import './App.css';
+import { React, useState } from "react";
+import "./App.css";
 import WhereTo from "./components/WhereTo";
 
 function App() {
+  
+  const [destinationData, setDestinationData] = useState({});
+  const [destinationName, setDestinationName] = useState("");
+
+  const setDestination = (data) => {
+    setDestinationData(data);
+    setDestinationName(data.properties.name);
+  }
+
   return (
     <div className="App">
       <h1 className="bg-red-400">
         Lets see if this branch works
       </h1>
-      <WhereTo />
-      
+      <h2>{destinationName}</h2>
+      <WhereTo setDestination={setDestination}/>
+
     </div>
   );
 }
--- src/components/WhereTo.js
+++ src/components/WhereTo.js
@@ -5,17 +5,18 @@ import '@geoapify/geocoder-autocomplete/styles/minimal.css'
 const API_KEY = "33b4af2cef534bd6b626019b07159028";
 let initialised = false;
 
-function WhereTo() {
+function WhereTo(props) {
+
+    const setDestination = props.setDestination;
 
     useEffect(() => {
 
         /*  
          *  From React 18, useEffect runs twice in development environment - 
          *  Could prevent this by disabling Strict mode in src/index.js, but 
-         *  preferring use of "initialised" conditional so we keep the development-only
-         *  checks performed in Strict mode
+         *  preferring use of "initialised" conditional so development-only
+         *  checks are still performed in Strict mode
         */                
-
         if(!initialised) { const autocomplete = new GeocoderAutocomplete(
                 document.getElementById("autocomplete"), 
                 API_KEY, 
@@ -25,16 +26,17 @@ function WhereTo() {
                     debounceDelay: 250
                 });
 
-            autocomplete.on('select', (location) => {
-            // check selected location here 
+            autocomplete.on("select", (destination) => {
+                // check selected location here 
+                setDestination(destination);
             });
 
-            autocomplete.on('suggestions', (suggestions) => {
+            autocomplete.on("suggestions", (suggestions) => {
             // process suggestions here
             });
             initialised = true;
         }
-    }, []);
+    }, [setDestination]);
       
     return (
         <div id="autocomplete" className="relative autocomplete-container"></div>
