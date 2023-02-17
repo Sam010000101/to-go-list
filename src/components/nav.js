ab0dd6a8f50f404733cdb3c4726537edf46b7238
tobias-firth <tobyfirth@btinternet.com>
2023-02-17 18:30:34 +0000

    add nav and footer

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,11 +4,11 @@ function Nav() {
     const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <div>
-      <nav id="nav" className="relative w-full flex flex-wrap items-center justify-between px-2 py-3 bg-slate-400 mb-3">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+      <nav id="nav" className="relative w-full flex flex-wrap items-center justify-between px-2 py-3 mb-3 bg-[#025]">
+        <div className="container px-3 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between md:w-auto md:static md:block md:justify-start">
-            <a className="text-3xl md:text-2xl font-bold inline-block mr-4 py-2 whitespace-nowrap  text-white" href="#">To-Go-List</a>
-            <button className="text-white cursor-pointer px-3 py-1 block md:hidden outline-none focus:outline-none text-4xl" type="button" onClick={() => setNavbarOpen(!navbarOpen)}> ≡ </button>
+            <a className="text-3xl md:text-2xl font-bold inline-block mr-3 py-2 whitespace-nowrap font-itim text-[#add8e6] lowercase" href="#">To-Go-List</a>
+            <button className="text- cursor-pointer px-3 py-1 block md:hidden outline-none focus:outline-none text-4xl" type="button" onClick={() => setNavbarOpen(!navbarOpen)}> ≡ </button>
           </div>
           <div
             className={
@@ -18,22 +18,22 @@ function Nav() {
           >
             <ul className="flex flex-col md:flex-row list-none md:ml-auto">
               <li className="nav-item">
-                <a className="px-3 py-2 flex items-center text-l  font-bold leading-snug text-white hover:underline decoration-4" href="#schedule">Schedule</a>
+                <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-l font-itim lowercase text-xl leading-snug text-[#add8e6] hover:underline decoration-wavy decoration-3" href="#schedule">Schedule</a>
               </li>
               <li className="nav-item">
-                <a className="px-3 py-2 flex items-center text-l  font-bold leading-snug text-white hover:underline decoration-4" href="#currency">Currency</a>
+                <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-l font-itim lowercase text-xl leading-snug text-[#add8e6] hover:underline decoration-wavy decoration-3" href="#currency">Currency</a>
               </li>
               <li className="nav-item">
-                <a className="px-3 py-2 flex items-center text-l  font-bold leading-snug text-white hover:underline decoration-4" href="#weather">Weather</a>
+                <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-l font-itim lowercase text-xl leading-snug text-[#add8e6] hover:underline decoration-wavy decoration-3" href="#weather">Weather</a>
               </li>
               <li className="nav-item">
-                <a className="px-3 py-2 flex items-center text-l  font-bold leading-snug text-white hover:underline decoration-4" href="#translator">Translator</a>
+                <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-l font-itim lowercase text-xl leading-snug text-[#add8e6] hover:underline decoration-wavy decoration-3" href="#translator">Translator</a>
               </li>
               <li className="nav-item">
-                <a className="px-3 py-2 flex items-center text-l  font-bold leading-snug text-white hover:underline decoration-4" href="#tips">Tips</a>
+                <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-l font-itim lowercase text-xl leading-snug text-[#add8e6] hover:underline decoration-wavy decoration-3" href="#tips">Tips</a>
               </li>
               <li className="nav-item">
-                <a className="px-3 py-2 flex items-center text-l  font-bold leading-snug text-white hover:underline decoration-4" href="#contact">Contact</a>
+                <a className=" hover:text-yellow-300 px-3 py-2 flex items-center text-l font-itim lowercase text-xl leading-snug text-[#add8e6] hover:underline decoration-wavy decoration-3" href="#contact">Contact</a>
               </li>
             </ul>
           </div>
@@ -43,7 +43,6 @@ function Nav() {
 )}
 
 export default Nav;
-
 
 
  
