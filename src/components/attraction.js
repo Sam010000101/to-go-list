af51fabba5fee1ae16ad95a454600504fb3740f0
Paul Ashby <paul@primitive.co>
2023-02-17 16:34:47 +0000

    Use ticks for attraction buttons

--- src/components/attraction.js
+++ src/components/attraction.js
@@ -5,10 +5,10 @@ function Attraction(props) {
     const place = props.properties;
 
     return (
-        <li className="text-left list-none bg-slate-100 rounded-md py-1 px-2">
-            <h2 className="text-lg font-semibold leading-6 mb-1">{place.name}</h2>
+        <li className="text-left list-none bg-slate-200 rounded-md py-1 px-2">
+            <h2 className="text-lg font-semibold leading-6 mb-1"><button onClick={() => {console.log("clicked");}} className="text-sm drop-shadow-sm">✅</button> {place.name}</h2>
             <p className="text-sm">{place.address_line2}</p>
-            <button onClick={() => {console.log("clicked");}} className="text-sm text-slate-50 bg-green-700 rounded-md py-1 px-2 my-2">Add</button>
+            
         </li>        
     )
 }
