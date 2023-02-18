dcb01e09c9d2551fadcfa7f4c231220e053c02cf
Paul Ashby <paul@primitive.co>
2023-02-18 01:41:43 +0000

    Replace Attraction button with Go on Day dropdown

--- src/components/attraction.js
+++ src/components/attraction.js
@@ -1,4 +1,5 @@
 import { React } from "react";
+import DayDropdown from "./DayDropdown";
 
 function Attraction(props) {
     
@@ -6,8 +7,9 @@ function Attraction(props) {
 
     return (
         <li className="text-left list-none bg-slate-200 rounded-md py-1 px-2">
-            <h2 className="text-lg font-semibold leading-6 mb-1"><button onClick={() => {console.log("clicked");}} className="text-sm drop-shadow-sm">✅</button> {place.name}</h2>
+            <h2 className="text-lg font-semibold leading-6 mb-1">{place.name}</h2>
             <p className="text-sm">{place.address_line2}</p>
+            <DayDropdown />
             
         </li>        
     )
