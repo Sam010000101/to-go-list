3f5ada450ffe20df13c493b6e18ee1e5d663df2f
Paul Ashby <paul@primitive.co>
2023-02-18 14:20:56 +0000

    Add day assignment menu to attraction

--- src/components/dayDropdown/index.js
+++ src/components/dayDropdown/index.js
@@ -3,75 +3,75 @@ import { Menu } from '@headlessui/react'
 function DayDropdown() {
   return (
     <Menu>
-      <Menu.Button>Go on</Menu.Button>
-      <Menu.Items>
+      <Menu.Button className="group mx-1 mr-1 leading-9"><span className="px-1 py-1 text-white rounded-sm bg-blue-500">Go</span><span className="hidden group-aria-expanded:inline text-slate-500"> on day </span></Menu.Button>
+      <Menu.Items className="inline mt-2">
         <Menu.Item>
           {({ active }) => (
             <a
-              className={`${active && 'bg-blue-500'}`}
+              className={`mx-1 px-2 py-1 text-sm text-white rounded-sm bg-slate-400 ${active && 'bg-blue-500'}`}
               href="/account-settings"
             >
-              Day 1
+             1
             </a>
           )}
         </Menu.Item>
         <Menu.Item>
           {({ active }) => (
             <a
-              className={`${active && 'bg-blue-500'}`}
+              className={`mx-1 px-2 py-1 text-sm text-white rounded-sm bg-slate-400 ${active && 'bg-blue-500'}`}
               href="/account-settings"
             >
-              Day 2
+              2
             </a>
           )}
         </Menu.Item>
         <Menu.Item>
           {({ active }) => (
             <a
-              className={`${active && 'bg-blue-500'}`}
+              className={`mx-1 px-2 py-1 text-sm text-white rounded-sm bg-slate-400 ${active && 'bg-blue-500'}`}
               href="/account-settings"
             >
-              Day 3
+              3
             </a>
           )}
         </Menu.Item>
         <Menu.Item>
           {({ active }) => (
             <a
-              className={`${active && 'bg-blue-500'}`}
+              className={`mx-1 px-2 py-1 text-sm text-white rounded-sm bg-slate-400 ${active && 'bg-blue-500'}`}
               href="/account-settings"
             >
-              Day 4
+              4
             </a>
           )}
         </Menu.Item>
         <Menu.Item>
           {({ active }) => (
             <a
-              className={`${active && 'bg-blue-500'}`}
+              className={`mx-1 px-2 py-1 text-sm text-white rounded-sm bg-slate-400 ${active && 'bg-blue-500'}`}
               href="/account-settings"
             >
-              Day 5
+              5
             </a>
           )}
         </Menu.Item>
         <Menu.Item>
           {({ active }) => (
             <a
-              className={`${active && 'bg-blue-500'}`}
+              className={`mx-1 px-2 py-1 text-sm text-white rounded-sm bg-slate-400 ${active && 'bg-blue-500'}`}
               href="/account-settings"
             >
-              Day 6
+              6
             </a>
           )}
         </Menu.Item>
         <Menu.Item>
           {({ active }) => (
             <a
-              className={`${active && 'bg-blue-500'}`}
+              className={`mx-1 px-2 py-1 text-sm text-white rounded-sm bg-slate-400 ${active && 'bg-blue-500'}`}
               href="/account-settings"
             >
-              Day 7
+              7
             </a>
           )}
         </Menu.Item>
