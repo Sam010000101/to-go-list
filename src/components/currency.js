921954befbf4dcb5923f4373ef3eb80aace51fdd
Sam B <reposme@outlook.com>
2023-02-20 21:10:50 +0000

    Further dev using states & test API

--- src/components/currency.js
+++ src/components/currency.js
@@ -1,47 +1,134 @@
-import React from "react";
-import DropdownBase from "./currencyDropdownBase";
-import DropdownExchange from "./currencyDropdownExchange";
-import CurrencyAPI from "./utils/APIs/CurrencyAPI";
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
+import Dropdown from 'react-dropdown';
+import { HiSwitchHorizontal } from 'react-icons/hi';
+import 'react-dropdown/style.css';
 
 function Currency() {
-    return (
-        <div id="currency" className="container mx-auto bg-slate-400 h-36 rounded-xl">
-                        <h1 className="text-lg font-semibold leading-6">CURRENCY CONVERTOR</h1>
-            <div className="flex flex-row justify-center pt-2">
-                <div className="w-1/3 mx-5">
-                    <label
-                        htmlFor="base-currency"
-                        className="block text-sm font-medium text-gray-700 pb-2">
-                        <h3 class="text-left">Base Currency</h3>
-                    </label>
-                    <div className="block text-sm font-medium text-gray-700 pb-1"><DropdownBase></DropdownBase></div>
-                    <input
-                        type="text"
-                        name="base-currency"
-                        id="base-currency"
-                        className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-4 pr-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-                        placeholder="Enter base currency" />
 
-                </div>
-                <div className="pt-7">
-                    <p>=</p>
-                </div>
-                <div className="w-1/3 mx-5">
-                    <label
-                        htmlFor="exchange-currency"
-                        className="block text-sm font-medium text-gray-700  pb-2">
-                        <h3 className="text-left">Exchange Currency</h3>
-                    </label>
-                    <div className="block text-sm font-medium text-gray-700 pb-1"><DropdownExchange></DropdownExchange></div>
-                    <div className="pt-2" id="exchange-currency">
-                        <h3 className="text-left">Currency Appears Here</h3>
-                    </div>
-                </div>
-            </div>
-        </div>
+// Initializing all the state variables
+const [info, setInfo] = useState([]);
+const [input, setInput] = useState(0);
+const [from, setFrom] = useState("usd");
+const [to, setTo] = useState("inr");
+const [options, setOptions] = useState([]);
+const [output, setOutput] = useState(0);
 
-        
-    )
+// Calling the api whenever the dependency changes
+useEffect(() => {
+	Axios.get(
+`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json`)
+.then((res) => {
+	setInfo(res.data[from]);
+	})
+}, [from]);
+
+// Calling the convert function whenever
+// a user switches the currency
+useEffect(() => {
+	setOptions(Object.keys(info));
+	convert();
+}, [info])
+	
+// Function to convert the currency
+function convert() {
+	var rate = info[to];
+	setOutput(input * rate);
+}
+
+// Function to switch between two currency
+function flip() {
+	var temp = from;
+	setFrom(to);
+	setTo(temp);
+}
+
+return (
+	<div className="App">
+	<div className="heading">
+		<h1>Currency converter</h1>
+	</div>
+	<div className="container">
+		<div className="left">
+		<h3>Amount</h3>
+		<input type="text"
+			placeholder="Enter the amount"
+			onChange={(e) => setInput(e.target.value)} />
+		</div>
+		<div className="middle">
+		<h3>From</h3>
+		<Dropdown options={options}
+					onChange={(e) => { setFrom(e.value) }}
+		value={from} placeholder="From" />
+		</div>
+		<div className="switch">
+		<HiSwitchHorizontal size="30px"
+						onClick={() => { flip()}}/>
+		</div>
+		<div className="right">
+		<h3>To</h3>
+		<Dropdown options={options}
+					onChange={(e) => {setTo(e.value)}}
+		value={to} placeholder="To" />
+		</div>
+	</div>
+	<div className="result">
+		<button onClick={()=>{convert()}}>Convert</button>
+		<h2>Converted Amount:</h2>
+		<p>{input+" "+from+" = "+output.toFixed(2) + " " + to}</p>
+
+	</div>
+	</div>
+);
 }
 
 export default Currency;
+
+
+// import React from "react";
+// import DropdownBase from "./currencyDropdownBase";
+// import DropdownExchange from "./currencyDropdownExchange";
+// import CurrencyAPI from "./utils/APIs/CurrencyAPI";
+
+// function Currency() {
+//     return (
+//         <div id="currency" className="container mx-auto bg-slate-400 h-36 rounded-xl">
+//                         <h1 className="text-lg font-semibold leading-6">CURRENCY CONVERTOR</h1>
+//             <div className="flex flex-row justify-center pt-2">
+//                 <div className="w-1/3 mx-5">
+//                     <label
+//                         htmlFor="base-currency"
+//                         className="block text-sm font-medium text-gray-700 pb-2">
+//                         <h3 class="text-left">Base Currency</h3>
+//                     </label>
+//                     <div className="block text-sm font-medium text-gray-700 pb-1"><DropdownBase></DropdownBase></div>
+//                     <input
+//                         type="text"
+//                         name="base-currency"
+//                         id="base-currency"
+//                         className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-4 pr-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+//                         placeholder="Enter base currency" />
+
+//                 </div>
+//                 <div className="pt-7">
+//                     <p>=</p>
+//                 </div>
+//                 <div className="w-1/3 mx-5">
+//                     <label
+//                         htmlFor="exchange-currency"
+//                         className="block text-sm font-medium text-gray-700  pb-2">
+//                         <h3 className="text-left">Exchange Currency</h3>
+//                     </label>
+//                     <div className="block text-sm font-medium text-gray-700 pb-1"><DropdownExchange></DropdownExchange></div>
+//                     <div className="pt-2" id="exchange-currency">
+//                         <h3 className="text-left">Currency Appears Here</h3>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+
+        
+//     )
+// }
+
+// export default Currency;
