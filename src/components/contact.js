aabbf28cf86164563c6481d4c7777d53d1792e91
tobias-firth <tobyfirth@btinternet.com>
2023-02-22 16:27:08 +0000

    add style

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,9 +2,14 @@ import React from "react";
 
 function Contact() {
     return (
-        <div id="contact" className="container mx-auto bg-slate-400 rounded-xl">
-        <h2>Get in Touch</h2>
-            <form name="contact" method="post">
+        <div id="contact" className="container mx-auto rounded-xl">
+         <span className="pt-3 gap-2 flex justify-center mx-1 mt-2  bg-[#025] rounded-t-xl rounded-b h-12">
+                <span className="font-itim relative text-blue-200 font-bold leading-6">Get in Touch</span>
+                <span className="inset-y-0 flex flex-col pl-2 pt-1 relative">
+                    <svg className="h-5 w-5 pb-1 fill-slate-300" viewBox="0 0 31.982 31.982">	<path d="M3.952,23.15L0,31.955l8.767-3.992l0.018,0.019L3.938,23.13L3.952,23.15z M4.602,22.463L24.634,2.432l4.849,4.848 L9.45,27.312L4.602,22.463z M30.883,0.941c-2.104-1.963-4.488-0.156-4.488-0.156l4.851,4.843 C31.244,5.627,33.124,3.375,30.883,0.941z" /></svg>
+                </span>
+            </span>
+            <form className="bg-slate-200" name="contact" method="post">
             <input type="hidden" name="form-name" value="contact" />
             <p>
                 <label htmlFor="name">Name</label> <br />
