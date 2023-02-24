fe6bc319918b4ec8c5ac35dee22c77f4192b5cf1
Paul Ashby <paul@primitive.co>
2023-02-24 10:46:07 +0000

    Use Pixabay API for images

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,31 +1,29 @@
 import { React, useEffect } from "react";
-import { createClient } from 'pexels';
+import PixabayAPI from "../utils/APIs/PixabayAPI";
 import Form from "../travel-form";
 import "./style.css";
 
 function Jumbotron(props) {
 
-    const API_KEY = "OVJEDExfeypBb1DqjkyDd3falp08tv5aCtvxRYFvMuuWxbv2DN8xSmyM";
-    const client = createClient(API_KEY);
     const destinationName = props.destinationName;
     const backgroundImage = props.backgroundImage;
     const setBackgroundImage = props.setBackgroundImage;
     const setDestination = props.setDestination;
 
-    useEffect(() => {        
-        client.photos.search({query: `${destinationName}`, orientation: "landscape", per_page: 1 })
+    useEffect(() => {
+        PixabayAPI.searchTerms(destinationName)
             .then(resp => {
-                if (resp.photos.length) {
-                    setBackgroundImage(resp.photos[0].src.large2x);
+                if (resp.data.hits.length) {
+                    setBackgroundImage(resp.data.hits[0].largeImageURL);
                 }
             });
-      }, [client, setBackgroundImage, destinationName]);
+}, [setBackgroundImage, destinationName]);
 
 
     return (
         <div id="jumbotron" className="relative rounded-xl container mx-auto h-72 bg-slate-400 overflow-hidden">
             <h1 className="absolute w-full font-itim text-[60px] text-white z-40">Where to?</h1>      
-            <div className="min-h-[300px] opacity-50 z-0 bg-center" style={{backgroundImage: `url(${backgroundImage})`}} alt="Evocative holiday scene"></div>
+            <div className="min-h-[300px] opacity-50 z-0 bg-cover bg-center" style={{backgroundImage: `url(${backgroundImage})`}} alt="Evocative holiday scene"></div>
             <Form setDestination={setDestination}/>
         </div>
     )
