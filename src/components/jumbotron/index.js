819cd7316437d18c30d1090404c1d1d2bdac59ed
Paul Ashby <paul@primitive.co>
2023-03-03 12:25:20 +0000

    Adjust jumbotron to allow for open menu

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -17,11 +17,11 @@ function Jumbotron({scrollToSearch, destinationName, backgroundImage, setBackgro
 
     return (
         <div ref={scrollToSearch} id="jumbotron" className="relative top-0 w-full h-screen bg-slate-400 overflow-hidden">
-            <div className="absolute top-36 w-full">
+            <div className="absolute top-12 w-full">
                 <h1 className="font-itim text-[60px] text-white text-center z-40">Where to?</h1>
                 <Form setDestination={setDestination}/>
             </div>
-            <div className="w-full h-screen bg-center bg-cover bg-[#025]" style={{backgroundImage: `linear-gradient(to top, transparent 40vh, #025), url(${backgroundImage})`}} id="main-image" alt="Evocative holiday scene"></div>
+            <div className="w-full h-screen bg-center bg-cover bg-[#025] -mt-18" style={{backgroundImage: `linear-gradient(to top, transparent 40vh, #025), url(${backgroundImage})`}} id="main-image" alt="Evocative holiday scene"></div>
         </div>
     )
 }
